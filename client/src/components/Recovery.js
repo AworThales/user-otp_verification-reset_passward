3d1355f7dac47c1647b97de042889b558fa0b8ad
AworThales <thals4jesus@gmail.com>
2023-07-02 08:18:41 -0700

    Updated file

--- client/src/components/Recovery.js
+++ client/src/components/Recovery.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import avatar from '../images/profile.png';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { passwordValidated } from '../helper/validate'
+// import { useAuthStore } from '../store/store'
 
-const Recovery = () => {
+import styles from '../styles/Username.module.css';
+
+export default function Recovery() {
+
   return (
-    <div>
-      Recovery
+    <div className="container py-1 mx-auto">
+
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+
+      <div className='flex justify-center py-0 items-center h-screen'>
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className='text-xl font-bold'>Password Recovery</h4>
+            <span className='py-1 text-xl w-2/3 text-center text-gray-500'>
+              Enter OTP to recover password
+            </span>
+          </div>
+
+          <form className='pt-20'>
+
+              <div className="textbox flex flex-col items-center gap-6">
+                    <div className='input text-center'>
+                    <span className='py-4 text-sm text-left text-gray-500'>
+                      Enter 6 digit OTP sent to your email address.
+                    </span>
+                  <input className={styles.textbox} type="text" placeholder='OTP' />
+                    </div>
+                  <button className={styles.btn} type='submit'>Recover</button>
+              </div>
+
+              <div className="text-center py-4">
+                <span className='text-gray-500'>Can't get OTP? <button className='text-red-500' to="/recovery">Resend</button></span>
+              </div>
+
+          </form>
+
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
-
-export default Recovery
--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import avatar from '../images/profile.png';
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { resetPasswordValidate } from '../helper/validate'
+// import { useAuthStore } from '../store/store'
 
-const Reset = () => {
+import styles from '../styles/Username.module.css';
+
+export default function Reset() {
+
+  // const navigate = useNavigate();
+  // const setUsername = useAuthStore(state => state.setUsername);
+
+  const formik = useFormik({
+    initialValues : {
+      password : '',
+      confirm_pass: ''
+    },
+    validate : resetPasswordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit : async values => {
+      console.log(values)
+    }
+  })
+
   return (
-    <div>
-      Reset
+    <div className="container py-1 mx-auto">
+
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+
+      <div className='flex justify-center py-0 items-center h-screen'>
+        <div className={styles.glass} style={{width:"50%"}}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className='text-xl font-bold'>Reset</h4>
+            <span className='py-1 text-xl w-2/3 text-center text-gray-500'>
+              Enter your password
+            </span>
+          </div>
+
+          <form className='py-20' onSubmit={formik.handleSubmit}>
+              
+              <div className="textbox flex flex-col items-center gap-6">
+                  <input value={formik.values.password} {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='New Password' />
+                  <input value={formik.values.password} {...formik.getFieldProps('confirm_pass')} className={styles.textbox} type="text" placeholder='Repeat Password' />
+                  <button className={styles.btn} type='submit'>Reset</button>
+              </div>
+          </form>
+
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
-
-export default Reset
