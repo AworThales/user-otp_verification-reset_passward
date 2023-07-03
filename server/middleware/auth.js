b95fe4d7770cbc54cab8981fa3338e5f08c7fe7a
AworThales <thals4jesus@gmail.com>
2023-07-03 09:45:39 -0700

    midleware for authentication update

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import ENV from "../config.js";
+
+// auth middleeware
+export default async function Auth(req, res, next){
+    try{
+
+        // Accessing authorize header to validate request
+        const token = req.headers.authorization.split(" ")[1];
+
+        // receiving the user details for the login user
+        const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
+
+        req.user = decodedToken;
+        next();
+
+    } catch (error) {
+        return res.status(401).send({ error : "Authenication failed!"})
+    }
+}
