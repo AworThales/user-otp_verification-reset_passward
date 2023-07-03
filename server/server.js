856a41d5f2552d4c67625ff1c9625ab01f697373
AworThales <thals4jesus@gmail.com>
2023-07-03 02:20:45 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import connect from './database/connection.js';
+import router from './router/route.js';
 
 const app = express();
 
@@ -18,9 +20,21 @@ app.get('/', (req,res) => {
     res.status(201).json("Home Get Request")
 });
 
+// My api routes
+app.use('/api', router)
 
-// Start server
-app.listen(port, () =>{
-    console.log(`Server connected to hhtp:localhost:${port}`);
-})
+// start the server when we have valid connection
+connect().then(() =>{
+    try {
+        
+        app.listen(port, () =>{
+        console.log(`Server connected to hhtp:localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error =>{
+    console.log("Invalid Database Connections")
+} )
+
 
