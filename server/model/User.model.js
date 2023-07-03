0559cf92af4c0fdd5fe00aae8498eb40ecc1d62d
AworThales <thals4jesus@gmail.com>
2023-07-03 02:47:14 -0700

    User Model updated

---
+++ server/model/User.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a unique Username"],
+        unique: [true, "Username already exist"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an Email"],
+        unique: true,
+    },
+    firstName: {type:String},
+    lastName: {type:String},
+    mobile: {type: Number},
+    address: {type:String},
+    profile: {String},
+})
+
+export default mongoose.model('User', UserSchema)
