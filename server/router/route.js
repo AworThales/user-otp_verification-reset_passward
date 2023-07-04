134f6e876d850e2e87b46609f9f0e7c70d5a5222
AworThales <thals4jesus@gmail.com>
2023-07-04 15:53:07 -0700

    Updated mailer route

--- server/router/route.js
+++ server/router/route.js
@@ -3,11 +3,12 @@ const router = Router();
 /** importinf all controllers */
 import * as controller from "../controllers/appController.js";
 import Auth, { localVariables } from "../middleware/auth.js";
+import { registerMail } from "../controllers/mailer.js";
 
 // Post Methods
 // router.route('/register').post((req,res) => res.json("register route")); //register user
 router.route('/register').post(controller.register); //register user
-// router.route('/registerMail').post(); //To send the email
+router.route('/registerMail').post(registerMail); //To send the email
 router.route("/authenticate").post((req,res) => res.end()); //To authenticate users
 router.route("/login").post(controller.verifyUser, controller.login); //login our app
 
